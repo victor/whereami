38d79dc2917c5d1d303fead8ae33885d8556ce81
Victor Jalencas <victor@victor.sh>
2014-09-02 23:17:02 +0200

    Initial empty project

---
+++ whereami/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  whereami
+//
+//  Created by Victor Jalencas on 02/09/14.
+//  Copyright (c) 2014 Hand Forged. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
